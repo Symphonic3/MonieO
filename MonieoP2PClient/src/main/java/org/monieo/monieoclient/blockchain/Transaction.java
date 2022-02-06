8b77bcce09162c7927faf352ff63c4731295cb1d
Symphonic3 <hermennelson@gmail.com>
2022-02-06 15:24:26 -0700

    finished create new transaction method

--- MonieoP2PClient/src/main/java/org/monieo/monieoclient/blockchain/Transaction.java
+++ MonieoP2PClient/src/main/java/org/monieo/monieoclient/blockchain/Transaction.java
@@ -25,11 +25,13 @@ public class Transaction extends AbstractTransaction {
 	
 	public static Transaction createNewTransaction(Wallet fundsOwner, WalletAdress to, BigDecimal amount, BigDecimal fee) {
 		
-		//TODO this
-		
 		BigInteger nonce = new BigInteger(String.valueOf(System.currentTimeMillis()) + String.valueOf(ThreadLocalRandom.current().nextInt()));
 		
-		return null;
+		TransactionData td = new TransactionData(Monieo.MAGIC_NUMBERS, Monieo.PROTOCOL_VERSION, fundsOwner.getAsWalletAdress(), to, amount, fee, Monieo.INSTANCE.getNetAdjustedTime(), nonce);
+		
+		String sig = fundsOwner.sign(td.serialize());
+		
+		return new Transaction(td, Monieo.serializeKeyPairPublic(fundsOwner.getKeyPair()), sig);
 		
 	}
 
