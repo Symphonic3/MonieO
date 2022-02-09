966afe54d4931a566295e9fa63a93dfdc0d63fbb
Symphonic3 <hermennelson@gmail.com>
2022-02-08 18:11:07 -0700

    tried to do thing with ui

--- MonieoP2PClient/src/main/java/org/monieo/monieoclient/gui/UI.java
+++ MonieoP2PClient/src/main/java/org/monieo/monieoclient/gui/UI.java
@@ -1,6 +1,7 @@
  package org.monieo.monieoclient.gui;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.Panel;
 import java.awt.Toolkit;
@@ -41,7 +42,10 @@ import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
 import javax.swing.ListSelectionModel;
+import javax.swing.LookAndFeel;
 import javax.swing.ScrollPaneLayout;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -64,13 +68,32 @@ public class UI {
 	private String[] walletNicks;
 	
 	public JList<String> list;
-	public JLabel addressLabel;
 	public JLabel LblADDRESSES;
 	public JButton btnChangeWalName;
 	public JButton btnDelWal;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	
+	JTextField lblNewLabel_1;
+	JPanel panel_1;
+	JPanel panel;
+	public JTextField addressLabel;
+	JTextField nickLabel;
+	JTextField INDIVbalanceLabel;
+	JScrollPane scrollPane;
+	JLabel label_1;
+	JLabel label_3;
+	JLabel label_5;
+	JButton sentTrnt;
+	JLabel lblNewLabel;
+	JLabel lblTransactionAmount;
+	JLabel lblFee;
+	JButton BtnNEWADDRESS;
+	JLabel lblToggleExperimentalMining;
+	JLabel lblTotalBalance;
+	JScrollPane scrollPane_1;
+	JLabel label;
 	
 	public UI() {
 	}
@@ -98,19 +121,19 @@ public class UI {
 
 		frame.getContentPane().add(TgBtnTOGGLEMINING);
 			
-		JTextField lblNewLabel_1 = new JTextField("Total addresses:");
+		lblNewLabel_1 = new JTextField("Total addresses:");
 		lblNewLabel_1.setEditable(false);
 		lblNewLabel_1.setOpaque(false); //this is the same as a JLabel
 		lblNewLabel_1.setBorder(null); //remove the border
 		lblNewLabel_1.setBounds(208, 411, 96, 34);
 		frame.getContentPane().add(lblNewLabel_1);
 		
-		JPanel panel_1 = new JPanel();
+		panel_1 = new JPanel();
 		panel_1.setLayout(null);
 		panel_1.setBounds(208, 0, 702, 400);
 		frame.getContentPane().add(panel_1);
 		
-		JTextField addressLabel = new JTextField("(address)");
+		addressLabel = new JTextField("(address)");
 		addressLabel.setEditable(false);
 		addressLabel.setOpaque(false); //this is the same as a JLabel
 		addressLabel.setBorder(null); //remove the border
@@ -155,7 +178,7 @@ public class UI {
 		
 		panel_1.add(addressLabel);
 		
-		JTextField nickLabel = new JTextField("(address nickname)");
+		nickLabel = new JTextField("(address nickname)");
 		nickLabel.setEditable(false);
 		nickLabel.setOpaque(false); //this is the same as a JLabel
 		nickLabel.setBorder(null); //remove the border
@@ -163,7 +186,7 @@ public class UI {
 		nickLabel.setBounds(144, 51, 331, 29);
 		panel_1.add(nickLabel);
 		
-		JTextField INDIVbalanceLabel = new JTextField("(address balance)");
+		INDIVbalanceLabel = new JTextField("(address balance)");
 		INDIVbalanceLabel.setEditable(false);
 		INDIVbalanceLabel.setOpaque(false); //this is the same as a JLabel
 		INDIVbalanceLabel.setBorder(null); //remove the border
@@ -171,7 +194,7 @@ public class UI {
 		INDIVbalanceLabel.setBounds(144, 91, 331, 29);
 		panel_1.add(INDIVbalanceLabel);
 		
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		panel.setBounds(10, 152, 663, 225);
 		panel_1.add(panel);
 		panel.setLayout(null);
@@ -245,28 +268,29 @@ public class UI {
 		
 		Refresh();
 		
-		JScrollPane scrollPane = new JScrollPane(list);
+		scrollPane = new JScrollPane(list);
 		scrollPane.setBounds(10, 11, 186, 434);
 		scrollPane.createVerticalScrollBar();
         scrollPane.setLayout(new ScrollPaneLayout());
 		frame.getContentPane().add(scrollPane);
 		
-		JLabel label_1 = new JLabel("Selected address:");
+		label_1 = new JLabel("Selected address:");
 		label_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		label_1.setBounds(10, 11, 106, 29);
 		panel_1.add(label_1);
 		
-		JLabel label_3 = new JLabel("Address nickname:");
+		label_3 = new JLabel("Address nickname:");
 		label_3.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		label_3.setBounds(10, 51, 133, 29);
 		panel_1.add(label_3);
 		
-		JLabel label_5 = new JLabel("Address balance:");
+		label_5 = new JLabel("Address balance:");
 		label_5.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		label_5.setBounds(10, 91, 133, 29);
 		panel_1.add(label_5);
 		
-		JButton sentTrnt = new JButton("Send transaction");
+		//what is a trnt and why is it already sent
+		sentTrnt = new JButton("Send transaction");
 		sentTrnt.setBounds(84, 191, 123, 23);
 		panel.add(sentTrnt);
 		sentTrnt.addActionListener(new ActionListener() {
@@ -295,11 +319,11 @@ public class UI {
 		panel.add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Recipient address:");
+		lblNewLabel = new JLabel("Recipient address:");
 		lblNewLabel.setBounds(84, 11, 117, 14);
 		panel.add(lblNewLabel);
 		
-		JLabel lblTransactionAmount = new JLabel("Transaction amount:");
+		lblTransactionAmount = new JLabel("Transaction amount:");
 		lblTransactionAmount.setBounds(84, 67, 117, 14);
 		panel.add(lblTransactionAmount);
 		
@@ -308,7 +332,7 @@ public class UI {
 		textField_1.setBounds(84, 92, 491, 20);
 		panel.add(textField_1);
 		
-		JLabel lblFee = new JLabel("Fee:");
+		lblFee = new JLabel("Fee:");
 		lblFee.setBounds(84, 120, 117, 14);
 		panel.add(lblFee);
 		
@@ -317,7 +341,7 @@ public class UI {
 		textField_2.setBounds(84, 145, 491, 20);
 		panel.add(textField_2);
 		
-		JButton BtnNEWADDRESS = new JButton("New address");
+		BtnNEWADDRESS = new JButton("New address");
 		BtnNEWADDRESS.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			    Object result = JOptionPane.showInputDialog(frame, "Enter new address nickname:");
@@ -332,26 +356,30 @@ public class UI {
 		BtnNEWADDRESS.setBounds(341, 417, 109, 23);
 		frame.getContentPane().add(BtnNEWADDRESS);
 		
-		JLabel lblToggleExperimentalMining = new JLabel("Toggle experimental mining (CPU only):");
+		lblToggleExperimentalMining = new JLabel("Toggle experimental mining (CPU only):");
 		lblToggleExperimentalMining.setBounds(474, 406, 188, 445);
 		frame.getContentPane().add(lblToggleExperimentalMining);
 		
-		JLabel lblTotalBalance = new JLabel("Total balance:");
-		JScrollPane scrollPane_1 = new JScrollPane();
+		lblTotalBalance = new JLabel("Total balance:");
+		scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(10, 11, 188, 429);
 		frame.getContentPane().add(scrollPane_1);
 		
 		lblTotalBalance.setBounds(758, 406, 76, 44);
 		frame.getContentPane().add(lblTotalBalance);
 		
-		JLabel label = new JLabel("Total balance:");
+		label = new JLabel("Total balance:");
 		label.setBounds(842, 406, 53, 44);
 		frame.getContentPane().add(label);
 				
-		frame.setVisible(true);
 		frame.setResizable(false);
 		
 		panel.setVisible(false);
+		
+		setDarkMode(false);
+		
+		frame.setVisible(true);
+		
 	}
 	void Refresh() {
 		
@@ -364,5 +392,31 @@ public class UI {
     	}
     	list.setListData(walletNicks);
     	LblADDRESSES.setText(Integer.toString(walletNicks.length));
+    	
 	}
+	
+	public void setDarkMode(boolean dark) {
+		
+		//TODO do this
+		
+		/*try {
+			//UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+		/*if (dark) {
+			
+			setColors(new Color(33, 33, 33), new Color(48, 48, 48), new Color(255, 255, 255));
+			
+		} else {
+			
+			setColors(new Color(222, 222, 222), new Color(255, 255, 255), new Color(0, 0, 0));
+			
+		}*/
+		
+	}
+	
 }
