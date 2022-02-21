966bd46e7aa39bc17ab8b9742225892602ef6232
Symphonic3 <hermennelson@gmail.com>
2022-02-21 15:55:07 -0700

    fix

--- MonieoP2PClient/src/main/java/org/monieo/monieoclient/networking/Node.java
+++ MonieoP2PClient/src/main/java/org/monieo/monieoclient/networking/Node.java
@@ -93,6 +93,7 @@ public class Node implements Runnable{
 			e1.printStackTrace();
 		}
 		Monieo.INSTANCE.nodes.remove(this);
+		Monieo.INSTANCE.ui.totConnectedNodesDisplay.setText(String.valueOf(Monieo.INSTANCE.nodes.size()));
 		kill = true;
 		
 	}
