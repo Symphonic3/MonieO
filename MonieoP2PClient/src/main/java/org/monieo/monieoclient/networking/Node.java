c89e9f0e1a956633083c537d30d5d9acc57f8552
feves caler <johnreed1223@gmail.com>
2022-03-07 20:06:25 -0700

    bassoon

--- MonieoP2PClient/src/main/java/org/monieo/monieoclient/networking/Node.java
+++ MonieoP2PClient/src/main/java/org/monieo/monieoclient/networking/Node.java
@@ -256,7 +256,7 @@ public class Node implements Runnable{
 
 				if (s == null || s.equals("")) continue;
 				
-				System.out.println("recieved data!");
+				//System.out.println("recieved data!");
 				
 				nc = NetworkPacket.deserialize(s);
 
@@ -290,10 +290,10 @@ public class Node implements Runnable{
 	
 	public boolean handle(NetworkPacket nc) {
 		
-		System.out.println("rec'd network command!" + nc.cmd);
+		/*System.out.println("rec'd network command!" + nc.cmd);
 		System.out.println("==BEGIN NETWORK COMMAND==");
 		System.out.println(nc.data);
-		System.out.println("==END NETWORK COMMAND==");
+		System.out.println("==END NETWORK COMMAND==");*/
 		
 		try {
 			
